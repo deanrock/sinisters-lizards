1a0248a6356cd6aa21fef1ff27fef343ef7d6a4e
Dejan Levec <dejan@celtra.com>
2020-12-18 09:48:09 +0100

    serve vue via api

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,18 @@
-# build stage
-FROM node:lts-alpine as api-stage
+FROM node:lts-alpine
 WORKDIR /app/api
 COPY api/package*.json ./
 RUN npm install
-COPY api/. .
 
-# build stage
-FROM node:lts-alpine as client-stage
 WORKDIR /app/client
 COPY client/package*.json ./
 RUN npm install
-COPY client/. .
-RUN npm run build
 
-# production stage
-FROM api-stage as production-stage
-COPY --from=client-stage /app/client/dist /dist
+WORKDIR /app/api
+COPY api/. .
+
+WORKDIR /app/client
+COPY client/. .
+
+WORKDIR /app/api
 EXPOSE 3000
-CMD node main.js
+CMD node app.js
--- api/app.js
+++ api/app.js
@@ -1,12 +1,10 @@
 const Koa = require('koa');
 const koaRouter = require("koa-router");
+var proxy = require('koa-proxy');
+const { exec } = require("child_process");
 
 const app = new Koa();
 const router = new koaRouter();
-
-router.get("/", (ctx) => {
-    ctx.body = "test";
-});
 
 router.get("/regions", async (ctx) => {
     ctx.body = "regions";
@@ -27,6 +25,28 @@ router.post("/tests", async (ctx) => {
 router.get("/tests/:id", async (ctx) => {
     ctx.body = ctx.params;
 });
+
+// Vue
+app.use(proxy({
+    host:  'http://localhost:8080',
+    match: /^\/(ui|css|js)\//
+}));
+
+exec("npm run serve", {
+    cwd: '../client/',
+}, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+// End Vue
 
 app.use(router.routes()).use(router.allowedMethods());
 
