ecaddda7028b268c731c3d910f51d22aecacc458
Kristian Remšak <kristian.remsak@celtra.com>
2020-12-18 09:50:44 +0100

    placeholders for config added, routes plan

--- api/app.js
+++ api/app.js
@@ -1,5 +1,16 @@
 const Koa = require('koa');
 const koaRouter = require("koa-router");
+const SSH2Promise = require('ssh2-promise');
+
+const sshconfig = {
+    host: '192.168.1.2',
+    username: 'ubuntu',
+    identity: '/here/is/my/key'
+  }
+
+const dbConfig = {
+
+}
 
 const app = new Koa();
 const router = new koaRouter();
@@ -8,25 +19,36 @@ router.get("/", (ctx) => {
     ctx.body = "test";
 });
 
+//return json with avaliable regions
 router.get("/regions", async (ctx) => {
     ctx.body = "regions";
 });
 
+//set regions
 router.post("/regions", async (ctx) => {
     ctx.body = "regions";
 });
 
+//return all tests (pending, error, finished)
 router.get("/tests", async (ctx) => {
     ctx.body = "regions";
 });
 
+//start new test - returns string:testId params:{regions:[], config{t: "", r: "", v:""}} 
 router.post("/tests", async (ctx) => {
     ctx.body = "regions";
 });
 
+//get test by id
 router.get("/tests/:id", async (ctx) => {
     ctx.body = ctx.params;
 });
+
+//establish ssh tunnel and runs test
+let runTest = async () => {
+ //establish shh tunel and run tests
+ //save results into database
+}
 
 app.use(router.routes()).use(router.allowedMethods());
 
