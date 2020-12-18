135287a73c5c4ae32e3fa394a31a2ac3841aba4e
Jure Malovrh <jure.malovrh@celtra.com>
2020-12-18 10:41:34 +0100

    Remove initial client code

--- client/src/components/TestsDetails.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    Page for listing details of a given test.
-
-    routes: GET API/test/:id (test details) --> get all wrk results from DB and displays them in some tab like structure. Would be nice if some basic graphs can be added.
-
-  </div>
-</template>
