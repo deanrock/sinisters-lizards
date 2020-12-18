dc3599501564c988fcb504be642a8846818b243b
Kristian Remšak <kristian.remsak@celtra.com>
2020-12-18 09:20:59 +0100

    basic routes

--- api/app.js
+++ api/app.js
@@ -1,8 +1,34 @@
 const Koa = require('koa');
-const app = new Koa();
+const koaRouter = require("koa-router");
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const app = new Koa();
+const router = new koaRouter();
+
+router.get("/", (ctx) => {
+    ctx.body = "test";
 });
 
-app.listen(3000);
+router.get("/regions", async (ctx) => {
+    ctx.body = "regions";
+});
+
+router.post("/regions", async (ctx) => {
+    ctx.body = "regions";
+});
+
+router.get("/tests", async (ctx) => {
+    ctx.body = "regions";
+});
+
+router.post("/tests", async (ctx) => {
+    ctx.body = "regions";
+});
+
+router.get("/tests/:id", async (ctx) => {
+    ctx.body = ctx.params;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
